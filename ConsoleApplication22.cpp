3aadc6b871bcb768b4be172843e94534b1357404
Nazar47Lysyk32 <137522819+Nazar47Lysyk32@users.noreply.github.com>
2023-09-24 17:37:37 +0300

    Add files via upload

---
+++ ConsoleApplication22.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+class MyStack {
+public:
+    
+    MyStack() {
+        topIndex = -1;
+    }
+
+    // Додавання елемента на верx стеку
+    void push(T value) {
+        if (topIndex < maxSize - 1) {
+            topIndex++;
+            stackData[topIndex] = value;
+        }
+        else {
+            cerr << "The stack is full." << endl;
+        }
+    }
+
+    // Видалення елемента з верxy стеку
+    void pop() {
+        if (!isEmpty()) {
+            topIndex--;
+        }
+        else {
+            cerr << "The stack is empty." << endl;
+        }
+    }
+
+    // Отримання верхнього елемента без видалення
+    T top() {
+        if (!isEmpty()) {
+            return stackData[topIndex];
+        }
+        else {
+            
+            return T();
+        }
+    }
+
+    // Перевірка, чи є стек порожнім
+    bool isEmpty() {
+        return topIndex == -1;
+    }
+
+private:
+    static const int maxSize = 100; 
+    T stackData[maxSize];
+    int topIndex;
+};
+
+int main() {
+    
+    MyStack<int> myStack;
+
+    
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+
+    
+    cout << "Верхній елемент: " << myStack.top() << endl;
+
+    // Видалення верхнього елемента
+    myStack.pop();
+
+    
+    cout << "Новий верхній елемент: " << myStack.top() << endl;
+
+    return 0;
+}
